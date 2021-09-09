d993acc7ca3aabb76b5d2defca64ad8b456bf470
Steffen Geving <GEVIS1@student.op.ac.nz>
2021-09-09 15:40:10 +1200

    Add docopt

--- Program.cs
+++ Program.cs
@@ -1,20 +1,38 @@
-﻿using System;
+﻿using DocoptNet;
+using System;
 
 namespace subnetcalculator
 {
     class Program
     {
+        private const string usage = @"Subnet Mask calulator.
+
+            Usage:
+              subnet.exe <ip> [mask]
+              subnet.exe 
+              subnet.exe 
+              subnet.exe 
+              subnet.exe (-h | --help)
+              subnet.exe --version
+
+            Options:
+              -h --help     Show this screen.
+              --version     Show version.
+              --speed=<kn>  Speed in knots [default: 10].
+              --moored      Moored (anchored) mine.
+              --drifting    Drifting mine.
+
+            ";
+
         static void Main(string[] args)
         {
-            if (args == null)
+            var arguments = new Docopt().Apply(usage, args, version: "Subnet Mask Calculator 0.1", exit: true);
+            foreach (var argument in arguments)
             {
-                PrintHelp();
+                Console.WriteLine("{0} = {1}", argument.Key, argument.Value);
             }
-        }
 
-        static void PrintHelp()
-        {
-
+            Console.ReadKey();
         }
     }
 }
