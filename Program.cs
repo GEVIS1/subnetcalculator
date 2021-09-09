562d07c82b1ae5ab343b14842b2152141f2dc1a5
Steffen Geving <GEVIS1@student.op.ac.nz>
2021-09-09 14:38:59 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace subnetcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                PrintHelp();
+            }
+        }
+
+        static void PrintHelp()
+        {
+
+        }
+    }
+}
